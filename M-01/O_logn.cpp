74ee3e65bc6e56d2e06e72c559e0d723a1edd7c0
MD NAZMUL ISLAM <md.nazmul.islam0087@gmail.com>
2024-06-09 00:08:35 +0600

    Osqrt n complexity

---
+++ M-01/O_logn.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n>0) // log N complexity
+    {
+        int digit = n%10;
+        cout << digit <<endl;
+        n/=10;
+    }
+
+    return 0;
+}
